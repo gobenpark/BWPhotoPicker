67f6390dc9dfd747db5b3e04e2fd64700a246e86
qkrqjadn <qjadn0914@naver.com>
2017-04-23 22:15:23 +0900

    code copy & paste

---
+++ Example/BWPhotoPicker/AssetViewController.swift
@@ -0,0 +1,118 @@
+//
+//  AssetViewController.swift
+//  sample
+//
+//  Created by 박범우 on 2017. 4. 23..
+//  Copyright © 2017년 bumwoo. All rights reserved.
+//
+
+import UIKit
+import Photos
+import PhotosUI
+
+import ALCameraViewController
+
+class AssetViewController: UIViewController{
+    
+    var asset: PHAsset!
+    var assetCollection: PHAssetCollection!
+    
+    var imageView = UIImageView()
+    var livePhotoView = PHLivePhotoView()
+    var isPlayingHint = false
+    
+    var targetSize: CGSize {
+        let scale = UIScreen.main.scale
+        return CGSize(width: imageView.bounds.width * scale,
+                      height: imageView.bounds.height * scale)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(livePhotoView)
+        
+        
+        
+        imageView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        livePhotoView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        livePhotoView.delegate = self
+        
+        // Make sure the view layout happens before requesting an image sized to fit the view.
+        view.layoutIfNeeded()
+        updateImage()
+
+    }
+    
+    func updateImage() {
+        if asset.mediaSubtypes.contains(.photoLive) {
+            updateLivePhoto()
+        } else {
+            updateStaticImage()
+        }
+    }
+    
+    func updateLivePhoto() {
+        // Prepare the options to pass when fetching the live photo.
+        let options = PHLivePhotoRequestOptions()
+        options.deliveryMode = .highQualityFormat
+        options.isNetworkAccessAllowed = true
+        
+        // Request the live photo for the asset from the default PHImageManager.
+        PHImageManager.default().requestLivePhoto(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { livePhoto, info in
+            // Hide the progress view now the request has completed.
+            
+            // If successful, show the live photo view and display the live photo.
+            guard let livePhoto = livePhoto else { return }
+            
+            // Now that we have the Live Photo, show it.
+            self.imageView.isHidden = true
+            self.livePhotoView.isHidden = false
+            self.livePhotoView.livePhoto = livePhoto
+            
+            if !self.isPlayingHint {
+                // Playback a short section of the live photo; similar to the Photos share sheet.
+                self.isPlayingHint = true
+                self.livePhotoView.startPlayback(with: .hint)
+            }
+            
+        })
+    }
+    
+    func updateStaticImage() {
+        // Prepare the options to pass when fetching the (photo, or video preview) image.
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .highQualityFormat
+        options.isNetworkAccessAllowed = true
+     
+        PHImageManager.default().requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { image, _ in
+            // Hide the progress view now the request has completed.
+            
+            // If successful, show the image view and display the image.
+            guard let image = image else { return }
+            
+            // Now that we have the image, show it.
+            self.livePhotoView.isHidden = true
+            self.imageView.isHidden = false
+            self.imageView.image = image
+        })
+    }
+}
+
+extension AssetViewController: PHLivePhotoViewDelegate {
+    func livePhotoView(_ livePhotoView: PHLivePhotoView, willBeginPlaybackWith playbackStyle: PHLivePhotoViewPlaybackStyle) {
+        isPlayingHint = (playbackStyle == .hint)
+    }
+    
+    func livePhotoView(_ livePhotoView: PHLivePhotoView, didEndPlaybackWith playbackStyle: PHLivePhotoViewPlaybackStyle) {
+        isPlayingHint = (playbackStyle == .hint)
+    }
+}
--- Example/BWPhotoPicker/ViewController.swift
+++ Example/BWPhotoPicker/ViewController.swift
@@ -7,18 +7,78 @@
 //
 
 import UIKit
+import Photos
+
 
 class ViewController: UIViewController {
-
+    
+    
+    let imgView = UIImageView()
+    
+    
+    lazy var photoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("사진으로 이동", for: .normal)
+        button.setTitleColor(.black,for: .normal)
+        button.addTarget(self, action: #selector(movePhoto), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(photoButton)
+        view.addSubview(imgView)
+        
+        photoButton.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        imgView.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.height.equalTo(imgView.snp.width)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func movecamera(){
+        let croppingEnabled = true
+        let cameraViewController = CameraViewController(croppingEnabled: croppingEnabled) { [weak self] image, asset in
+            // Do something with your image here.
+            // If cropping is enabled this image will be the cropped version
+            
+            self?.dismiss(animated: true, completion: nil)
+        }
+        
+        present(cameraViewController, animated: true, completion: nil)
+    }
+    func movePhoto2(){
+        let croppingEnabled = true
+        
+        /// Provides an image picker wrapped inside a UINavigationController instance
+        let imagePickerViewController = CameraViewController.imagePickerViewController(croppingEnabled: croppingEnabled) { [weak self] image, asset in
+            self?.imgView.image = image
+            
+            self?.dismiss(animated: true, completion: nil)
+        }
+        
+        present(imagePickerViewController, animated: true, completion: nil)
+    }
+    
+    func movePhoto(){
+        self.navigationController?.pushViewController(PhotoViewController(), animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
+
+
 
